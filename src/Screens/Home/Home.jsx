70fd0f2af11c9bfcdac8a6bc715b21ae5f00489b
Dhinesh4668 <dhinesh4668@gmail.com>
2023-04-22 09:26:39 +0530

    home page design

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -7,10 +7,15 @@ const Home = () => {
       <div className='home-container'>
         <div className='home-left'>
           <div className='home-title'>
-            <h1>Hai, I'm Dhinesh Kumar</h1>
+            <h1>Hai, I'm <span>Dhinesh Kumar</span></h1>
           </div>
-          <div></div>
-          <div></div>
+          <div>
+            <p>some content </p>
+          </div>
+          <div className='home-btn'>
+              <button>button</button>
+              <button>button</button>
+          </div>
         </div>
         <div className='home-right-img'>
           <img src='src/assets/react.svg' alt='' />
